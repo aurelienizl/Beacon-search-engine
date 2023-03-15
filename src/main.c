59f7fb0aff734f927278f0b9ad0385a7a85aa9f0
Aurelien Izoulet <aurelien.izoulet@epita.fr>
2023-03-15 18:58:26 +0100

    added crawling loop

--- src/main.c
+++ src/main.c
@@ -9,70 +9,45 @@
 #include "structures/string/string.h"
 #include "structures/stack/stack.h"
 
-void init_db(char* url)
+void init_db(char *url)
 {
-   struct stack *stack = new_stack();
-   addstack(stack, url);
-   while(!is_empty(stack))
-   {
-       char* url = unstack(stack);
-       if(exist_webpage(url, strlen(url)))
-       {
-           free(url);
-           continue;
-       }
-       char* data = crawl(url);
-       struct webpage webpage = create_webpage(url, strlen(url), data, strlen(data));
-       write_webpage(&webpage);
+    struct stack *stack = new_stack();
+    addstack(stack, url);
+    while (!is_empty_stack(stack))
+    {
+        char *url = unstack(stack);
+        if (exist_webpage(url, strlen(url)))
+        {
+            printf("Website %s already crawled\n", url);
+            free(url);
+            continue;
+        }
+        printf("Crawling %s\n", url);
+        char *data = crawl(url);
+        struct webpage webpage = create_webpage(url, strlen(url), data, strlen(data));
+        printf("Writing %s\n", webpage.url);
+        write_webpage(&webpage);
 
-        // Analyse des liens de page 
-        // J'ajoute dans la pile 
+        // Analyse des liens de page
+        // J'ajoute dans la pile
         // Je continue
 
-       free(url);
-       free(data);
-       free(webpage.url);
-       free(webpage.content->data);
-       free(webpage.content);
-       free(webpage.checksum);
-   }
+        free(url);
+        free(data);
+        free(webpage.url);
+        free(webpage.content->data);
+        free(webpage.content);
+        free(webpage.checksum);
+    }
     free(stack);
 }
 
-
 void tests()
 {
-    
-    char* url1 = string_to_heap("http://www.citizendium.org/wiki/Welcome_to_Citizendium", 52);
 
-    // Crawle the page
-    char* data = crawl(url1);
+    char *url1 = string_to_heap("http://www.citizendium.org/wiki/Welcome_to_Citizendium", 52);
 
-    // Store to server
-    struct webpage webpage = create_webpage(url1, strlen(url1), data, strlen(data));
-    write_webpage(&webpage);
-
-    // Check if the folder is properly created
-    int res = exist_webpage(webpage.url, webpage.url_size);
-    printf("The folder is created: %d\n", res);
-
-    // Get webpage 
-    unsigned char* content = read_webpage(webpage.url, webpage.url_size);
-
-    // Print page 
-    printf("%s\n", content);
-
-    // Free memory
-
-    free(content);
-    free(url1);
-    free(data);
-    free(webpage.url);
-    free(webpage.content->data);
-    free(webpage.content);
-    free(webpage.checksum);
-
-    
+    init_db(url1);
 }
 
 int main()
