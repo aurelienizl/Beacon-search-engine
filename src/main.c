05286879a034f03ea0f82bc34c64cfc1802d9e21
Aurelien Izoulet <aurelien.izoulet@epita.fr>
2023-03-23 15:25:00 +0100

    Updated tests

--- src/main.c
+++ src/main.c
@@ -11,62 +11,65 @@
 //#include "indexer/indexer.h"
 #include "indexer/link_extractor.h"
 
-void init_db(char *url)
+char *get_domain_ext(char *str)
 {
-    struct stack *stack = new_stack();
-    addstack(stack, url);
-    while (!is_empty_stack(stack))
-    {
-        char *url = unstack(stack);
-        printf("Crawling %s\n", url);
-        if (exist_webpage(url, strlen(url)))
-        {
-            printf("Website %s already crawled\n", url);
-            free(url);
-            continue;
-        }
-        char *data = crawl(url);
-        struct webpage webpage = create_webpage(url, strlen(url), data, strlen(data));
-        printf("Writing %s\n", webpage.url);
-        write_webpage(&webpage);
-
-        // Print the length of the webpage
-        printf("Length of webpage: %ld\n", strlen(data));
-
-        // Print the content of the webpage
-        char *content = (char *)read_webpage(url, strlen(webpage.url));
-        printf("Content of webpage: %s\n\n\n\n", content);
-
-        //struct list *url_list = new_list();
-	//bparser(&url_list, data, url, strlen(data));
-    
-	//printf("\n%s\n", (char *)url_list->next->data);
-	
-
-        //free(content);
-        free(url);
-        free(data);
-        free(webpage.url);
-        free(webpage.content->data);
-        free(webpage.content);
-        free(webpage.checksum);
-    }
-    free(stack);
+    const char *prefix = "://";
+    const char *start = strstr(str, prefix);
+    if (start == NULL)
+        return NULL;
+    start += strlen(prefix);
+    const char *end = strchr(start, '/');
+    if (end == NULL)
+        end = str + strlen(str);
+    size_t length = end - start;
+    char *domain = malloc(length + 1);
+    if (domain == NULL)
+        return NULL;
+    strncpy(domain, start, length);
+    domain[length] = '\0';
+    return domain;
 }
 
-void tests()
+void new_test()
 {
+    // Get the url
+    char *url = string_to_heap("http://en.citizendium.org/");
 
-    char *url1 = string_to_heap("http://en.citizendium.org/wiki/Welcome_to_Citizendium");
-    printf("url1: %s\n", url1);
+    // TODO: get the domain name
+    char *domain = get_domain_ext(url);
 
-    init_db(url1);
+    // Create the stack
+    struct stack *stack = new_stack();
+    // Add the url to the stack
+    addstack(stack, url);
+   
+    // While the stack is not empty
+    while(!is_empty_stack(stack))
+    {
+        char *url = unstack(stack);
+        // Check if the url is already in the database
+        if(read_webpage(url, strlen(url)) != NULL)
+        {
+            continue;
+        }
+        // Get the content of the url
+        char* content = get_content(domain, url);
+        if(content == NULL)
+        {
+            continue;
+        }
+        // Store the content in the database
+        struct webpage page = create_webpage(url, strlen(url), content, strlen(content));
+        write_webpage(&page);
+
+        // TODO: get the links
+    }
 }
 
 int main()
 {
     // test crawler
-    tests();
+    new_test();
 
     return 0;
 }
